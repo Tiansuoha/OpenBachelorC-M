aad9dfe3da7e039b5aa17135cb0c4e746bf95b26
Tiansuoha <112594254+Tiansuoha@users.noreply.github.com>
2025-10-03 10:52:04 +0800

    Delete main_no_proxy.sh

--- main_no_proxy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# OpenBachelorC Main Script (No Proxy) for Android/Termux
-# Equivalent of main_no_proxy.cmd
-
-python -m pipx run poetry run main --no-proxy
-echo "Press Enter to continue..."
-read
